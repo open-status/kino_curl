298b5857451d4d186a41a159b87c2c454837fc48
Daniel Rudnitski <danielrudn@gmail.com>
2022-12-26 18:17:54 -0500

    initial commit

---
+++ lib/kino_curl.ex
@@ -0,0 +1,18 @@
+defmodule KinoCurl do
+  @moduledoc """
+  Documentation for `KinoCurl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KinoCurl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kino_curl_test.exs
@@ -0,0 +1,8 @@
+defmodule KinoCurlTest do
+  use ExUnit.Case
+  doctest KinoCurl
+
+  test "greets the world" do
+    assert KinoCurl.hello() == :world
+  end
+end
